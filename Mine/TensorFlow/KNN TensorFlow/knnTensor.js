a4fbd68ce4dc994d5444483a5ebbfda43ae04543
Maxi <tholem98@gmail.com>
2019-09-15 15:30:59 -0300

    add ing the error

--- Mine/TensorFlow/KNN TensorFlow/knnTensor.js
+++ Mine/TensorFlow/KNN TensorFlow/knnTensor.js
@@ -3,15 +3,22 @@ const tf = require('@tensorflow/tfjs')
 const loadCSV = require('./load-csv')
 
 function knn(features,labels,predictionPoint,k){
-    features
-        .sub(predictionPoint)
+    const {mean,variance} = tf.moments(features,0)
+    const scalePrediction = predictionPoint.sub(mean).div(variance.pow(0.5))
+
+    let arrayTensor = features
+        .sub(mean)
+        .div(variance.pow(.5))
+        .sub(scalePrediction)
         .pow(2)
         .sum(1)
         .pow(.5)
         .expandDims(1)
         .concat(labels,1)
         .unstack()
-        .sort((a,b) => {console.log(a);return a.get(0)>b.get(0)?1:-1})
+
+        return arrayTensor
+        .sort((a,b) => a.get(0)>b.get(0)?1:-1)
         .slice(0,k)
         .reduce((acc,pair)=> acc + pair[1],0) / k
 }
@@ -19,13 +26,14 @@ function knn(features,labels,predictionPoint,k){
 let {features,labels,testFeatures,testLabels} = loadCSV('houseData.csv',{
     shuffle: true,
     splitTest: 10,
-    dataColumns: ['lat','long'],
+    dataColumns: ['lat','long','sqft_lot','sqft_living'],
     labelColumns: ['price']
 })
 
 features = tf.tensor(features)
 labels = tf.tensor(labels)
 testFeatures.forEach((testPoint,i)=>{
+    console.log(testPoint)
 const result = knn(features,labels,tf.tensor(testPoint),10)
 const err = (testLabels[i][0] - result)/testLabels[i][0]
 console.log(testLabels[i][0],result)
