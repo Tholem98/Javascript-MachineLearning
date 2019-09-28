c514eb88a479c360677a7907c8d542190da424fb
Maxi <tholem98@gmail.com>
2019-09-28 16:00:21 -0300

    images

---
+++ Mine/TensorFlow/Images/image.js
@@ -0,0 +1,23 @@
+require('@tensorflow/tfjs-node')
+const tf = require('@tensorflow/tfjs')
+const MultinominalLogisticRegression = require('../Regresion Linear/Clasificativa/multinominal')
+const LogisticRegression = require('../Regresion Linear/Clasificativa/logisticRegression')
+const _ = require('lodash')
+const mnist = require('mnist-data')
+
+const mnistData = mnist.training(0,10)
+
+const features = mnistData.images.values.map(image => _.flatMap(image))
+const encodedLabels = mnistData.labels.values.map(label =>{
+    const row = new Array(10).fill(0)
+    row[label] = 1
+    return row;
+})
+
+const regression = new MultinominalLogisticRegression(features, encodedLabels,{
+    learningRate: 1,
+    iterations:5,
+    batchSize: 100
+})
+
+regression.train()
